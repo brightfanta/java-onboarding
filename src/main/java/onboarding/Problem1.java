d17ad0955c451970d00fbc486a6718dda25e76ba
hwanhee <brightfanta@gmail.com>
2023-09-19 17:10:25 +0900

    20230919 precourse study

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,9 +1,39 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+
+        //난수 생성(1~400)
+
+        //예외 처리: 시작면이나 마지막면 나오면 책 다시 펼치기
+
+        //왼쪽페이지(홀수) 숫자를 각자리 숫자로 쪼개기
+
+        // 각자리 숫자의 합 또는 곱 최대값 찾기
+
+        //오른쪽페이지(짝수) 숫자를 각자리 숫자로 쪼개기
+
+        // 각자리 숫자의 합 또는 곱 최대값 찾기
+
+        //왼쪽 페이지 최대값과 오른쪽페이지 최대값 비교 후 최종 최대값 선정
+
+        //포비와 크롱 각각에 대해 최종 최대값 선정
+
+        //각 값을 비교 후 결과 출력(1, 0, -1)
+
+
+
+        //왼쪽이 작은 홀수, 오른쪽이 큰홀수
+
+        //pobi crong 의 길이는 2
+
+
+        pobi = new ArrayList<>();
+
+        crong = new ArrayList<>();
+
         int answer = Integer.MAX_VALUE;
         return answer;
     }
--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,7 +1,21 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
+
+        //문자입력 받기(길이가 1~1000/알파벳 소문자로만 구성)
+        //문자열로 반환
+        //반복하면서 연속되는 숫자 삭제
+        //연속하는 숫자 없을 경우 최종 결과 출력
+
+        Scanner sc = new Scanner(System.in);
+        cryptogram = sc.next();
+
+
+
         String answer = "answer";
         return answer;
     }
