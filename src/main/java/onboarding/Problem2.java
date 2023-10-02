5e69bebde10c518bae189f7c6ce8a8ef5ab7c537
hwanhee <brightfanta@gmail.com>
2023-10-02 22:49:18 +0900

    20231002 precourse study

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -6,17 +6,38 @@ import java.util.Scanner;
 public class Problem2 {
     public static String solution(String cryptogram) {
 
-        //문자입력 받기(길이가 1~1000/알파벳 소문자로만 구성)
-        //문자열로 반환
-        //반복하면서 연속되는 숫자 삭제
-        //연속하는 숫자 없을 경우 최종 결과 출력
+        /**
+         * 문자입력 받기(길이가 1~1000/알파벳 소문자로만 구성)
+         * length 1001부터는 예외처리 해버린다.
+         * try-catch 예외처리 한다.
+         *
+         * 문자열로 반환
+         * 반복하면서 연속되는 숫자 삭제
+         * 연속하는 숫자 없을 경우 최종 결과 출력
+         */
+        boolean flag = true;
 
-        Scanner sc = new Scanner(System.in);
-        cryptogram = sc.next();
 
+        do {
+            for (int i = 0; i < cryptogram.length() - 1; i++) {
+                if(cryptogram.charAt(i) == cryptogram.charAt(i + 1)) {
+                    System.out.println("cryptogram before substring = " + cryptogram);
+                    cryptogram = cryptogram.substring(0, i) + cryptogram.substring(i + 2);
+                    System.out.println("cryptogram after substring = " + cryptogram);
+                    break;
+                }
+                if (i == cryptogram.length() - 2) {
+                    flag = false;
+                    break;
+                }
+            }
 
+        } while (cryptogram.length() != 0 && flag);
 
-        String answer = "answer";
+        System.out.println("final cryptogram = " + cryptogram);
+        System.out.println();
+
+        String answer = cryptogram;
         return answer;
     }
 }
--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,14 +2,38 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-
+        
+        
         //Character ASCII코드 숫자 갖는 사항 활용
         //대문자 소문자 입력
         //(출력 알파벳 코드) = (Z or z의 알파벳 코드 - (해당 알파벳 코드 - (A or a의 알파벳 코드))
         //nextLine으로 입력 받기
         //알파벳일 경우에만 변환! 개행문자는 무시
 
+        String answer = "";
+        char temp = ' ';
+
+        while (true) {
+            if (!(word.length() >= 1 && word.length() <= 1_000)) {
+                return answer;
+            } else {
+                for (int i = 0; i < word.length(); i++) {
+                    if((int)word.charAt(i) >= 65 && (int)word.charAt(i) <= 90){
+//                temp = word.charAt(i);
+                        temp = (char) (90 - ((int) word.charAt(i) - 65));
+                        word = word.substring(0, i) + temp + word.substring(i + 1);
+                    }
+
+                    if((int)word.charAt(i) >= 97 && (int)word.charAt(i) <= 122){
+//                temp = word.charAt(i);
+                        temp = (char) (122 - ((int) word.charAt(i) - 97));
+                        word = word.substring(0, i) + temp + word.substring(i + 1);
+                    }
+                }
+                answer = word;
+                break;
+            }
+        }
         return answer;
     }
 }
