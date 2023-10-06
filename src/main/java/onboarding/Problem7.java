02136cd3f1c682d4b43ca9139d87e1d583e61a81
brightfanta <brightfanta@gmail.com>
2023-10-06 21:52:34 +0900

    20231006 precourse study

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,7 +1,7 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
@@ -22,7 +22,91 @@ public class Problem7 {
         //friends랑 visitor 합쳐서 list<String, Integer>(key, value) 만들기 & 중복 제거 value 0으로 초기화
         //list<String, Integer>의 Integer에 요소별 누적합 / visitors 있으면 +1 / friends의 조건 만족시 +10
 
-        List<String> answer = Collections.emptyList();
+        List<String> friend = new ArrayList<>();
+        List<String> friendAbility = new ArrayList<>();
+        HashMap<String, Integer> recordList = new HashMap<>();
+        List<String> visitorsCnt = new ArrayList<>();
+        List<String> answer = new ArrayList<>();
+        int record = 0;
+
+        //친구 추리기
+        for (List<String> relation : friends) {
+            for (int i = 0; i < relation.size(); i++) {
+                if (relation.get(i).equals(user)) {
+                    for(String r : relation) {
+                        if (!(r.equals(user))) {
+                            friend.add(r);
+                        }
+                    }
+                }
+            }
+        }
+        //친구가능성 추리기
+        for (List<String> relation : friends) {
+            for (String r : relation) {
+                for (String f : friend) {
+                    if (r.equals(f)) {
+                        for(String r2 : relation) {
+                            if (!(r2.equals(f) || r2.equals(user))) {
+                                friendAbility.add(r2);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        //친구가능성 점수 계산
+        for (String fA : friendAbility) {
+            record = 10;
+            if (recordList.containsKey(fA)) {
+                record = recordList.get(fA);
+                record += 10;
+                recordList.remove(fA);
+                recordList.put(fA, record);
+            }
+            recordList.put(fA, record);
+        }
+
+        //방문객 점수 계산
+        for (String v : visitors) {
+            record = 1;
+            if (recordList.containsKey(v)) {
+                record = recordList.get(v);
+                record += 1;
+                recordList.remove(v);
+                recordList.put(v, record);
+            }
+            recordList.put(v, record);
+        }
+        for (int i = 0; i < recordList.size(); i++) {
+            for (String f : friend) {
+                if (recordList.containsKey(f)) {
+                    recordList.remove(f);
+                }
+            }
+        }
+
+        //점수 오름차순 정렬
+        List<Map.Entry<String, Integer>> entries =
+                recordList.entrySet().stream()
+                        .sorted(Map.Entry.comparingByValue())
+                        .collect(Collectors.toList());
+
+        List<Map.Entry<String, Integer>> entryList = new LinkedList<>(recordList.entrySet());
+        entryList.sort(new Comparator<Map.Entry<String, Integer>>() {
+            @Override
+            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                return o2.getValue() - o1.getValue();
+            }
+        });
+
+        for (Map.Entry<String, Integer> entry : entryList) {
+            System.out.println("Key: " + entry.getKey() + ", "
+                    + "Value: " + entry.getValue());
+            answer.add((String) entry.getKey());
+
+        }
         return answer;
     }
 }
